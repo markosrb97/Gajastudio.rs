ec25e4ed68e7ded641a1b1561d23d3c8f159183e
Filip <andricfilip97@gmail.com>
2021-02-15 16:55:34 +0100

    update

---
+++ src/main/resources/static/assets/js/product.js
@@ -0,0 +1,12 @@
+function maxValue(value) {
+    
+    if ((value <= 10 && value >= 1 && !isNaN(value)) || value == "") {
+        
+       
+       }
+    else {
+        $("#kolicina").val(1);
+    }
+    
+
+}
