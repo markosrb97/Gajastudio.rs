c0c072d26b4f82af42691d8ae6b314531b4032cf
Filip <andricfilip97@gmail.com>
2021-02-16 11:02:04 +0100

    update

--- src/main/resources/assets/js/contactForm.js
+++
@@ -1,8 +0,0 @@
-var max = 1000;
-var textArea = $(".polje");
-var wordsLeft = $(".broj-reci");
-
-function maxWords() {
-    let currentWords = textArea.val().length;
-    wordsLeft.html(max-currentWords);
-}
--- src/main/resources/assets/js/orderForm.js
+++
@@ -1,8 +0,0 @@
-var max = 500;
-var textArea = $(".napomena");
-var wordsLeft = $(".duzina-poruke");
-
-function maxWords(){
-    let currentWords = textArea.val().length;
-    wordsLeft.html(max-currentWords);
-}
