e2ee9011e37a4a14559cda1d54539cd82fa5be2d
Filip <andricfilip97@gmail.com>
2021-02-17 15:58:25 +0100

    update

--- src/main/java/rs/gaiastudio/web/AdminController.java
+++ src/main/java/rs/gaiastudio/web/AdminController.java
@@ -1,11 +1,15 @@
 package rs.gaiastudio.web;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import rs.gaiastudio.model.Candle;
 import rs.gaiastudio.repo.CandleRepository;
@@ -29,6 +33,40 @@ public class AdminController {
 		ArrayList<Candle> candles = (ArrayList<Candle>) products.findAll();
 		model.addAttribute("candles", candles);
 		return "adminPage";
+	}
+	
+	@GetMapping("/admin/add")
+	public String addItem(Model model) {
+		
+		return "add";
+	}
+	
+	@PostMapping("/admin/add")
+	public String addItem(@ModelAttribute Candle candle) {
+		
+		System.out.println("adding " + candle.toString());
+		products.save(candle);
+		
+		return "redirect:/admin";
+	}
+	
+	
+	@GetMapping("/admin/edit/{id}")
+	public String editItem(Model model, @PathVariable int id) {
+		
+		Optional<Candle> item = products.findById((long) id);
+		Candle candle = item.get();
+		model.addAttribute("candle", candle);
+		
+		return "edit";
+	}
+	
+	@PostMapping("/admin/edit/{id}")
+	public String editItem(Model model, @PathVariable int id, @ModelAttribute Candle candle) {
+		
+		//TODO
+		
+		return "edit";
 	}
 	
 	/*	TODO:
