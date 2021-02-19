4d4cfa37971eae960f32ff0dc320b582a911ed9f
Filip <andricfilip97@gmail.com>
2021-02-19 14:32:58 +0100

    update

--- src/main/java/rs/gaiastudio/model/Candle.java
+++ src/main/java/rs/gaiastudio/model/Candle.java
@@ -11,14 +11,37 @@ public class Candle {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
+	
 	@Column(name = "type")
 	private String type;
+	
 	@Column(name = "price")
 	private int price;
-	@Column(name = "description", columnDefinition = "CLOB")
+	
 	@Lob
+	@Column(name = "description"/*, columnDefinition = "CLOB"*/)
 	private String description;
 	
+	@Lob
+	@Column(name="image1")
+	private byte[] imageFile1;
+	
+	@Lob
+	@Column(name="image2")
+	private byte[] imageFile2;
+	
+	
+	
+	
+	public Candle(String type, int price, String description, byte[] imageFile1, byte[] imageFile2) {
+		super();
+		this.type = type;
+		this.price = price;
+		this.description = description;
+		this.imageFile1 = imageFile1;
+		this.imageFile2 = imageFile2;
+	}
+
 	public Candle(String type, int price, String description) {
 		this.type = type;
 		this.price = price;
@@ -58,6 +81,22 @@ public class Candle {
 		this.description = description;
 	}
 	
+	public byte[] getImageFile1() {
+		return imageFile1;
+	}
+
+	public void setImageFile1(byte[] imageFile1) {
+		this.imageFile1 = imageFile1;
+	}
+	
+	public byte[] getImageFile2() {
+		return imageFile2;
+	}
+
+	public void setImageFile2(byte[] imageFile2) {
+		this.imageFile2 = imageFile2;
+	}
+
 	public long getId() {
 		return this.id;
 	}
--- src/main/java/rs/gaiastudio/web/AdminController.java
+++ src/main/java/rs/gaiastudio/web/AdminController.java
@@ -1,5 +1,9 @@
 package rs.gaiastudio.web;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Optional;
 
@@ -10,6 +14,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import rs.gaiastudio.model.Candle;
 import rs.gaiastudio.repo.CandleRepository;
@@ -17,6 +23,9 @@ import rs.gaiastudio.repo.CandleRepository;
 
 @Controller
 public class AdminController {
+	
+	private String UPLOAD_FOLDER1 = System.getProperty("user.dir") + "/target/classes/static/assets/images/1/";
+	private String UPLOAD_FOLDER2 = System.getProperty("user.dir") + "/target/classes/static/assets/images/2/";
 	
 	@Autowired
 	CandleRepository products;
@@ -30,6 +39,7 @@ public class AdminController {
 	@GetMapping("/admin")
 	public String adminPage(Model model) {
 		
+		
 		ArrayList<Candle> candles = (ArrayList<Candle>) products.findAll();
 		model.addAttribute("candles", candles);
 		return "adminPage";
@@ -37,15 +47,32 @@ public class AdminController {
 	
 	@GetMapping("/admin/add")
 	public String addItem(Model model) {
-		
-		return "add";
+		model.addAttribute("candle", new Candle());
+		return "adminAdd";
 	}
 	
 	@PostMapping("/admin/add")
-	public String addItem(@ModelAttribute Candle candle) {
+	public String addItem(@ModelAttribute Candle candle, @RequestParam("file1") MultipartFile file1, @RequestParam("file2") MultipartFile file2) {
+		
+		try {
+			candle.setImageFile1(file1.getBytes());
+			candle.setImageFile2(file2.getBytes());
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 		
 		System.out.println("adding " + candle.toString());
 		products.save(candle);
+		Path path1 = Paths.get(UPLOAD_FOLDER1 + candle.getType() + ".jpg");
+		Path path2 = Paths.get(UPLOAD_FOLDER2 + candle.getType() + ".jpg");
+		System.out.println(path1);
+        try {
+			Files.write(path1, candle.getImageFile1());
+			Files.write(path2, candle.getImageFile2());
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
 		
 		return "redirect:/admin";
 	}
@@ -58,7 +85,7 @@ public class AdminController {
 		Candle candle = item.get();
 		model.addAttribute("candle", candle);
 		
-		return "edit";
+		return "adminEdit";
 	}
 	
 	@PostMapping("/admin/edit/{id}")
