667937b9166822a883ba97e9c2c264db686db1f5
obrad97 <janiobrad@gmail.com>
2021-02-21 02:24:59 +0100

    update za orderForm

--- src/main/resources/assets/js/orderForm.js
+++ src/main/resources/assets/js/orderForm.js
@@ -1,8 +1,25 @@
 var max = 500;
 var textArea = $(".napomena");
 var wordsLeft = $(".duzina-poruke");
+var prekidac = 0;
+
+$(window).resize(function() {
+    if( $(this).width() > 1100 ) {
+      $('.meni-spusten').hide();
+    }
+  });
 
 function maxWords(){
     let currentWords = textArea.val().length;
     wordsLeft.html(max-currentWords);
+}
+
+function showHideMenu() {
+    if (prekidac == 0) {
+        $(".meni-spusten").slideDown();
+        prekidac = 1;
+      } else {
+        $(".meni-spusten").slideUp();
+        prekidac = 0;
+      }
 }
